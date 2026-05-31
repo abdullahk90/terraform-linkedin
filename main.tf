43e9bae5c598a444975fe6e71bc4cce5ba55b46a
Abdullah K <44611138+abdullahk90@users.noreply.github.com>
2026-05-31 09:16:30 +0530

    updated main.tf to configure autoscaling group

--- main.tf
+++ main.tf
@@ -80,13 +80,25 @@ resource "aws_lb_target_group_attachment" "blog" {
   port             = 80
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.2.1"
 
-  tags = {
-    Name = "Learning Terraform"
+  name = "blog"
+
+  min_size = 1
+  max_size = 1
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+
+  launch_template_name = "blog"
+  security_groups      = [module.blog_sg.security_group_id]
+  instance_type        = var.instance_type
+  image_id             = data.aws_ami.app_ami.id
+
+  traffic_source_attachments = {
+    blog-alb = {
+      traffic_source_identifier = aws_lb_target_group.blog.arn
+    }
   }
 }
