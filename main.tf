eac5ef52f67a88384f9d2209055db5e0ff5d5abf
Abdullah K <44611138+abdullahk90@users.noreply.github.com>
2026-05-31 08:02:37 +0530

    updated main.tf file

--- main.tf
+++ main.tf
@@ -29,11 +29,15 @@ resource "aws_instance" "blog" {
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
+  name        = "blog"
+  description = "Allow http and https in. Allow everything out."
+
+  vpc_id = data.aws_vpc.default.id
+
   tags = {
     Terraform = "true"
   }
-  vpc_id = data.aws_vpc.default.id
+  
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
@@ -42,6 +46,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.blog.id
 }
 
@@ -52,6 +57,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.blog.id
 }
 
@@ -62,5 +68,6 @@ resource "aws_security_group_rule" "blog_everything_out" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
+  
   security_group_id = aws_security_group.blog.id
 }
